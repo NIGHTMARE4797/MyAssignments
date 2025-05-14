c57b9cc2fa4e9a7182ab8f8ba2dc223eea4ad794
NIGHTMARE4797 <30741606+NIGHTMARE4797@users.noreply.github.com>
2025-05-14 22:41:26 +0530

    Add files via upload
    Day1 hw

---
+++ Homework.java
@@ -0,0 +1,25 @@
+package sel;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version=91.0f;
+String name="Google";
+boolean type=false;
+int year=2008;
+char key='C';
+
+System.out.println("versionoffloattype:"+version);
+System.out.println("name:"+name+"\n" + "releaseyear:"+ year+"\n"+"isBetaoftypebooleans:"+type+"\n"+ "shortcutkey: "+key);
+	
+int data=10;
+if(data>0)
+	System.out.println("Positive Number");
+else 
+	System.out.println("Negative Number");
+	
+	}
+
+}
